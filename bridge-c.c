27e887d01eaf9177531fcc334ba2f431c981c643
Alan Cox <alan@linux.intel.com>
2015-11-10 21:18:15 +0000

    C helpers continued

---
+++ bridge-c.c
@@ -0,0 +1,7 @@
+/*
+ * 
+ *	Game database follows below. Although linked into the same asm
+ *	file to make life easier this is just "mere aggregation" for
+ *	convenience, due to limits in the tool chain and the game licence
+ *	not the GPL applies below this point
+ */
---
+++ imsg-c.c
@@ -0,0 +1,35 @@
+
+const char toomuch[] = { "I am carrying too much. " };
+const char dead[] = { "I am dead.\n" };
+const char stored_msg[] = { "I have stored " };
+const char stored_msg2[] = { "treasures. On a scale of 0 to 100, that rates " };
+const char dotnewline[] = { ".\n" };
+const char newline[] = { "\n" };
+const char carrying[] = { "I am carrying:\n" };
+const char dashstr[] = { " - " };
+const char nothing[] = { "nothing" };
+const char lightout[] = { "My light has run out." };
+const char lightoutin[] = { "My light runs out in " };
+const char turns[] = { "turns" };
+const char turn[] = { "turn" };
+const char whattodo[] = { "\nTell me what to do ? " };
+const char prompt[] = { "> " };
+const char dontknow[] = { "You use word(s) I don't know! " };
+const char givedirn[] = { "Give me a direction too. " };
+const char darkdanger[] = { "Dangerous to move in the dark! " };
+const char brokeneck[] = { "I fell down and broke my neck. " };
+const char cantgo[] = { "I can't go in that direction. " };
+const char dontunderstand[] = { "I don't understand your command. " };
+const char notyet[] = { "I can't do that yet. " };
+const char beyondpower[] = { "It is beyond my power to do that. " };
+const char okmsg[] = { "O.K. " };
+const char whatstr[] = { "What ? " };
+const char itsdark[] = { "I can't see. It is too dark!" };
+const char youare[] = { "I am in a " };
+const char nonestr[] = { "none" };
+const char obexit[] = { "\nObvious exits: " };
+const char canalsosee[] = { "I can also see: " };
+const char playagain[] = { "Do you want to play again Y/N" };
+const char invcond[] = { "INVCOND" };
+const char *exitmsgptr = { "North", "South", "East", "West", "Up", "Down" };
+
