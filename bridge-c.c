c0cd27bd1b4094624529e007e73d1b7d98d6e35a
Alan Cox <alan@linux.intel.com>
2016-03-22 22:39:50 +0000

    bridge-c: clarify

--- bridge-c.c
+++ bridge-c.c
@@ -3,5 +3,5 @@
  *	Game database follows below. Although linked into the same asm
  *	file to make life easier this is just "mere aggregation" for
  *	convenience, due to limits in the tool chain and the game licence
- *	not the GPL applies below this point
+ *	not the GPL applies to the game database.
  */
